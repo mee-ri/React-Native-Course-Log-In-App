012c12e56fcf870db2401ca6b93fead5b9207e76
Meeri Klemola <ac0502@student.jamk.fi>
2022-10-23 20:44:26 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as SelfPractice} from './app.json';
+
+AppRegistry.registerComponent('SelfPractice', () => App);
